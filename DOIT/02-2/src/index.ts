d42f86af913169fa76dca8ae8e2179e6eace6f9c
이준희 <junh0328@naver.com>
2021-03-17 21:54:21 +0900

    setting: CRA 타입스크립트 템플릿을 이용한 리액트 초기세팅

--- DOIT/02-2/src/index.ts
+++ DOIT/02-2/src/index.ts
@@ -1,7 +1,7 @@
-import IPerson from './person/IPerson';
-import Person from './person/Person';
-import Chance from 'chance';
-import * as R from 'ramda';
+import IPerson from './person/IPerson'; // 타입들을 모아놓은 인터페이스의 기능을 하는 IPerson
+import Person from './person/Person'; // IPerson 인터페이스를 통해 생성자를 만든 객체 Person
+import Chance from 'chance'; // faker와 같은 기능을 하는 랜덤한 이름을 출력해주는 라이브러리
+import * as R from 'ramda'; // Math 오브젝트 보다 간단하고 편한 기능을 제공하는 라이브러리
 
 const chance = new Chance();
 let persons: IPerson[] = R.range(0, 2).map((n: number) => new Person(chance.name(), chance.age()));
