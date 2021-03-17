9107f7767934706be8cd93154af57943c4fb3642
이준희 <junh0328@naver.com>
2021-03-17 13:46:49 +0900

    DOIT/ 모듈화

--- DOIT/02-2/src/index.ts
+++ DOIT/02-2/src/index.ts
@@ -1,3 +1,10 @@
-import { testMakePerson } from './utils/makePerson';
+import IPerson from './person/IPerson';
+import Person, { makePerson } from './person/Person';
+
+const testMakePerson = (): void => {
+  let jane: IPerson = makePerson('Jane');
+  let jack: IPerson = makePerson('Jack');
+  console.log(jane, jack);
+};
 
 testMakePerson();
