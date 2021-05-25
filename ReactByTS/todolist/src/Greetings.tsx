af90dfd3887192c1695ce01c1ce21acbc851f016
이준희 <junh0328@naver.com>
2021-05-25 17:25:04 +0900

    fix: 타입스크립트 개념 오타 및 마크다운 정리

--- ReactByTS/todolist/src/Greetings.tsx
+++ ReactByTS/todolist/src/Greetings.tsx
@@ -2,28 +2,23 @@ import React from 'react';
 
 type GreetingsProps = {
   name: string;
-  mark: string;
   optional?: string;
   onClick: (name: string) => void;
 };
 
-const Greetings = ({ name, mark, optional, onClick }: GreetingsProps) => {
+const Greetings = ({ name, optional, onClick }: GreetingsProps) => {
   const handleClick = () => {
     onClick(name);
   };
   return (
     <div>
-      Hello, {name} {mark}
+      Hello, {name}
       {optional && <p> {optional}</p>}
       <div>
         <button onClick={handleClick}>Click Me</button>
       </div>
     </div>
   );
-};
-
-Greetings.defaultProps = {
-  mark: '!',
 };
 
 export default Greetings;
